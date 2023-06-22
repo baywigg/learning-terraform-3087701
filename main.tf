ee87610c0f6230add7af775efc353fe404d4640b
baywigg <117475958+baywigg@users.noreply.github.com>
2023-06-22 15:01:57 -0700

    pls

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ module "vpc" {
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  enable_nat_gateway = true
+  enable_nat_gateway = false
 
   tags = {
     Terraform = "true"
@@ -47,7 +47,7 @@ module "blog_sg" {
   version = "5.1.0"
   name    = "blog_new"
 
-  vpc_id = module.vpc.vpc_id
+  vpc_id = module.vpc.public_subnets[0]
 
   ingress_rules = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
