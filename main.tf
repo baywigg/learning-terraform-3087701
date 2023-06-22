d334c122e85eb62774cab286aa553c7befd69430
baywigg <117475958+baywigg@users.noreply.github.com>
2023-06-22 15:03:07 -0700

    updated version

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ module "vpc" {
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  enable_nat_gateway = false
+  enable_nat_gateway = true
 
   tags = {
     Terraform = "true"
@@ -44,7 +44,7 @@ resource "aws_instance" "blog" {
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "5.1.0"
+  version = "4.13.0"
   name    = "blog_new"
 
   vpc_id = module.vpc.public_subnets[0]
